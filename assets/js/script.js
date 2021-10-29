39ebc902f51640e38addee64d775d328fc6136d5
StevieinWaterford <stevieindub@gmail.com>
2021-10-29 14:35:58 +0000

    Add js functionality to gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-// Show cart
+let current = document.querySelector('#current');
+let imgs = document.querySelectorAll('.imgs img');
+let opacity = 0.5;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+
+function imgClick(e) {
+//Change clicked image current image
+    current.src = e.target.src;
+
+//Change the opacity 
+    e.target.style.opacity = opacity;
+
+}
+
+
+
+
+
+ 
+ 

0f22bc7acd0180b1c305e829433a2e23c478fef4
StevieinWaterford <stevieindub@gmail.com>
2021-11-08 19:55:15 +0000

    Add functionality to photo gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,18 @@
 let current = document.querySelector('#current');
 let imgs = document.querySelectorAll('.imgs img');
-let opacity = 0.5;
+let opacity = 0.4;
+
 
 imgs.forEach(img => img.addEventListener('click', imgClick));
 
-
 function imgClick(e) {
-//Change clicked image current image
     current.src = e.target.src;
-
-//Change the opacity 
-    e.target.style.opacity = opacity;
-
 }
 
 
 
 
 
- 
- 
+
+
+
